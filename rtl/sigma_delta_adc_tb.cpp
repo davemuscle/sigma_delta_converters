faf62d5b736a383bb4703b98042be097d9864a41
davemuscle <>
2022-01-26 22:54:59 -0500

    what an absolute nightmare trying to pass the design generics to verilator. best way I could figure out was exporting environment variables in bash

--- rtl/sigma_delta_adc_tb.cpp
+++ rtl/sigma_delta_adc_tb.cpp
@@ -1,4 +1,6 @@
 #include "Vsigma_delta_adc.h"
+#include "Vsigma_delta_adc_sigma_delta_adc.h"
+
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
@@ -6,9 +8,10 @@ class Harness {
 public:
     Vsigma_delta_adc *dut = new Vsigma_delta_adc;
     VerilatedVcdC *m_trace = new VerilatedVcdC;
-    int glb_cycles = 0;
-    int events = 0;
+    uint64_t glb_cycles = 0;
+    uint64_t events = 0;
 
+    //handler for waveform tracing
     void trace(std::string cmd = "dump"){
         //open vcd file 
         if(!cmd.compare("start")){
@@ -24,6 +27,7 @@ public:
         }
     }
 
+    //clk stim
     void clock(void){
         this->dut->clk ^= 1;
         this->dut->eval();
@@ -32,36 +36,56 @@ public:
         }
     }
 
-    void reset(){
-        //temp
+    //reset dut
+    void reset(int start, int end){
         if(this->dut->clk == 1){
-            if(this->glb_cycles > 4 && this->glb_cycles < 11){
+            if(this->glb_cycles >= start && this->glb_cycles <= end){
                 this->dut->rst = 1;
             } else {
                 this->dut->rst = 0;
             }
         }
     }
-        
+    float vcc;
+    float scale;
+    //generate sine input to dut as a pulse density modulated signal
+    int generate_input(int start, int freq){
+        //float analog = cos((2*M_PI*freq*glb->cycles)/);
+        return 0;
+    }
+
+
 };
 
 const int MAX_SIM_TIME = 50;
 
+
+//#define BOSR 256
+#define TEST 100
+
 int main(int argc, char **argv, char **env){
+
 
     Verilated::commandArgs(argc, argv);
     Verilated::traceEverOn(true);
 
     Harness *hns = new Harness;
-
+    //printf("CPP BOSR: %d\n", hns->dut->var_BOSR);
+    //printf("CPP BOSR: %d\n", hns->dut->VCC);
+    const char *p = std::getenv("BOSR");
+    printf("\n\n\n");
+    printf("got BOSR: %s\n", p);
     hns->trace("start");
 
     // tb
     
+    hns->glb_cycles = 0;
+
     while(hns->glb_cycles < MAX_SIM_TIME){
 
         hns->clock();
-        hns->reset();
+        hns->reset(5, 10);
+        hns->generate_input(0, 1);
         hns->trace();
 
     } 
@@ -70,7 +94,7 @@ int main(int argc, char **argv, char **env){
 
     while(hns->glb_cycles < MAX_SIM_TIME){
         hns->clock();
-        hns->reset();
+        hns->reset(5, 10);
         hns->trace();
     }
 
