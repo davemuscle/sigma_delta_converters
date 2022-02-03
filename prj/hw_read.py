4b037ad5a3365a6a2d63c552739ef2e3a4d9d3a5
davemuscle <>
2022-02-02 21:27:27 -0500

    pretty good SNR measurement added

--- prj/hw_read.py
+++ prj/hw_read.py
@@ -33,6 +33,7 @@ class HwTest:
         self.FPGA_bosr          = bosr
         self.FPGA_cic_stages    = cic_stages
         self.FPGA_bclk          = bclk
+        self.FPGA_sclk          = bclk / bosr
         self.FPGA_bits          = bits
         self.FPGA_vcc           = vcc
         self.FPGA_signed_output = signed_output
@@ -53,13 +54,17 @@ class HwTest:
         self.WVFM_sweep_steps = sweep_steps
         self.WVFM_sweep_mult  = sweep_mult
         self.WVFM_dft_size    = dft_size
+        self.WVFM_dft_delta   = self.FPGA_sclk / self.WVFM_dft_size
+        self.WVFM_dft_fbin    = round(self.WVFM_freq / self.WVFM_dft_delta)
 
+    # Print out parameters for debug
     def dump_params(self):
         print('-'*20 + " FPGA Parameters " + '-'*20)
         print(f"*  {self.FPGA_num_samples   = }")
         print(f"*  {self.FPGA_bosr          = }")
         print(f"*  {self.FPGA_cic_stages    = }")
         print(f"*  {self.FPGA_bclk          = }")
+        print(f"*  {self.FPGA_sclk          = }")
         print(f"*  {self.FPGA_bits          = }")
         print(f"*  {self.FPGA_vcc           = }")
         print(f"*  {self.FPGA_signed_output = }")
@@ -73,6 +78,7 @@ class HwTest:
         print(f"*  {self.WVFM_sweep_steps = }")
         print(f"*  {self.WVFM_sweep_mult  = }")
         print(f"*  {self.WVFM_dft_size    = }")
+        print(f"*  {self.WVFM_dft_fbin    = }")
 
     # Use Digilent Device
     def open_dad(self):
@@ -91,7 +97,6 @@ class HwTest:
     # Read lines from FPGA serial port, can lock up easily
     def read_serial(self):
         ser = serial.Serial(self.FPGA_uart, self.FPGA_baud)
-        # has to match FPGA
         ser.write(b's')
         lines = []
         for i in range(self.FPGA_num_samples):
@@ -154,32 +159,31 @@ class HwTest:
                 XQ[k] = XQ[k] + samples[n] * (math.cos(2*3.14*k*n/self.WVFM_dft_size))
                 XI[k] = XI[k] + samples[n] * -1 * (math.sin(2*3.14*k*n/self.WVFM_dft_size))
 
-        XM = [0]*((self.WVFM_dft_size>>1)-1)
-        Xf = [0]*((self.WVFM_dft_size>>1)-1)
+        XM   = [0]*((self.WVFM_dft_size>>1)-1)
+        Xf   = [0]*((self.WVFM_dft_size>>1)-1)
+        XM_l = [0]*len(XM)
 
         sample_freq = self.FPGA_bclk / self.FPGA_bosr
 
         for k in range((self.WVFM_dft_size>>1)-1):
             Xf[k] = k * sample_freq / self.WVFM_dft_size
-            XM[k] = math.sqrt(XQ[k+1]**2 + XI[k+1]**2) / self.WVFM_dft_size
+            XM[k] = math.sqrt(XQ[k+1]**2 + XI[k+1]**2) / (self.WVFM_dft_size/2)
+            XM_l[k] = 20*math.log10(XM[k] / hw.WVFM_amp)
 
-        return Xf, XM
-    
-    # Crude SFDR function
-    def get_sfdr(self, mags):
-        highest = mags[0]
-        highest_idx = 0
-        highest2nd = mags[0]
-        highest2nd_idx = 0
-        for i in range(1,len(mags)):
-            if(mags[i] > highest):
-                highest_idx = i
-                highest = mags[i]
-        for i in range(0,len(mags)):
-            if(i != highest_idx and mags[i] > highest2nd):
-                highest2nd = mags[i]
-                highest2nd_idx = i
-        return highest, highest2nd
+        return Xf, XM, XM_l
+
+    # Get frequency closest to DFT bin 
+    def match_dft_bin_freq(self):
+        self.WVFM_freq = self.WVFM_dft_fbin * self.WVFM_dft_delta
+
+    # Get signal-to-noise ratio via DFT magnitudes
+    def get_snr(self, mags):
+        idx = self.WVFM_dft_fbin - 1
+        top = mags[idx]
+        mags[idx] = 0
+        low = self.get_rms(mags)
+        mags[idx] = top
+        return 20*math.log10(top/low)
 
 
 
@@ -192,82 +196,173 @@ def filter(samples, samp_freq):
     taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
     return lfilter(taps, 1.0, samples)
 
+def test_normal(hw):
+    # config device, setup wavegen
+    hw.open_dad()
+    hw.setup_dad_waveform()
+    time.sleep(0.1)
+    # read sample buffer, convert to voltage
+    samples = hw.decode_serial(hw.read_serial())
+    voltages = hw.get_voltages(samples)
+    # get output results
+    amplitude = hw.get_amplitude(voltages)
+    dc = hw.get_dc(voltages)
+    rms = hw.get_rms(voltages)
+    dft_freqs, dft_mags, dft_mags_log = hw.get_dft(voltages)
+    # show params
+    hw.dump_params()
+    # print results
+    print('-'*20 + " Normal Test Result " + '-'*20)
+    print("*  Amp: " + str(amplitude))
+    print("*   DC: " + str(dc))
+    print("*  RMS: " + str(rms))
+    print("*  Max: " + str(max(voltages)))
+    print("*  Min: " + str(min(voltages)))
+    # plot
+    subplot(211)
+    plot(voltages)
+    grid()
+    title("Hardware Data")
+    xlabel('Samples')
+    ylabel('Voltage(V)')
+    subplot(212)
+    plot(dft_freqs, dft_mags_log)
+    xscale('log')
+    grid()
+    title("FFT")
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB)')
+    tight_layout()
+    show()
+    hw.close_dad()
 
-def oneshot_run(freq, amp, uart_device, num_samples, signed=0, filter=0):
-
-    if(filter):
-        samples_filtered = filter(samples, samp_freq)
-        Xf_f, XM_f = dft(samples_filtered, num_samples, samp_freq)
-        fig,axs = subplots(2,1)
-        axs[0].plot(samples_filtered)
-        axs[0].set_title("Hardware Data Filtered")
-        axs[1].plot(Xf_f, XM_f)
-        axs[1].set_title("FFT")
-
-
-#def bode_plot(samples, num_samples, samp_freq, bosr, vcc, filter):
-def oneshot(hw):
+def test_measurement(hw):
     # config device
     hw.open_dad()
-    #hw.setup_dad_waveform()
-    
-    samp_clk = hw.FPGA_bclk / hw.FPGA_bosr
-    custom_len = round(samp_clk / hw.WVFM_freq)
+    # get the next available frequency based on FFT
+    hw.match_dft_bin_freq();
+
+    # record ambient noise
+    ambient_samples = hw.decode_serial(hw.read_serial())
+
+    # create clean signal then record it
+    custom_len = round(hw.FPGA_sclk / hw.WVFM_freq)
     hw.dad.setup_custom_data(custom_len)
-    # create noise signal
+    for i in range(custom_len):
+        hw.dad.custom_data[i] = math.cos(2*3.14*hw.WVFM_freq*i/hw.FPGA_sclk)
+    hw.dad.wavegen_config_custom_out(0, hw.WVFM_freq, hw.WVFM_amp, 0)
+    time.sleep(0.1)
+    clean_samples = hw.decode_serial(hw.read_serial())
+
+    # add noise clean signal then record it
     noise_amp = 0.1
     for i in range(custom_len):
         noise = noise_amp*random.randrange(-100,100,1)/100
         hw.dad.custom_data[i] += noise
+    hw.dad.wavegen_config_custom_out(0, hw.WVFM_freq, hw.WVFM_amp, 0)
+    time.sleep(0.1)
+    dirty_samples = hw.decode_serial(hw.read_serial())
+   
+    # gather measurements
+    ambient_voltages = hw.get_voltages(ambient_samples)
+    clean_voltages = hw.get_voltages(clean_samples)
+    dirty_voltages = hw.get_voltages(dirty_samples)
+
+    ambient_amp = hw.get_amplitude(ambient_voltages)
+    clean_amp   = hw.get_amplitude(clean_voltages)
+    dirty_amp   = hw.get_amplitude(dirty_voltages)
     
-    # get rms of the noise only
-    hw.dad.wavegen_config_custom_out(0, hw.WVFM_freq, hw.WVFM_amp, 0)
-    time.sleep(0.1)
-    samples = hw.decode_serial(hw.read_serial())
-    noise_voltages = hw.get_voltages(samples)
-    noise_rms = hw.get_rms(noise_voltages)
+    ambient_dc = hw.get_dc(ambient_voltages)
+    clean_dc   = hw.get_dc(clean_voltages)
+    dirty_dc   = hw.get_dc(dirty_voltages)
 
-    # add in the signal (sine wave)
-    for i in range(custom_len):
-        hw.dad.custom_data[i] += math.cos(2*3.14*hw.WVFM_freq*i/samp_clk)
+    ambient_rms = hw.get_rms(ambient_voltages)
+    clean_rms   = hw.get_rms(clean_voltages)
+    dirty_rms   = hw.get_rms(dirty_voltages)
 
-    hw.dad.wavegen_config_custom_out(0, hw.WVFM_freq, hw.WVFM_amp, 0)
-    time.sleep(0.1)
-    #hw.dad.wavegen_config_sine_out(freq=hw.WVFM_freq, amp=hw.WVFM_amp)
+    ambient_dft_freqs, ambient_dft_mags, ambient_dft_mags_log = hw.get_dft(ambient_voltages)
+    clean_dft_freqs, clean_dft_mags, clean_dft_mags_log       = hw.get_dft(clean_voltages)
+    dirty_dft_freqs, dirty_dft_mags, dirty_dft_mags_log       = hw.get_dft(dirty_voltages)
 
-    # read sample buffer
-    samples = hw.decode_serial(hw.read_serial())
-    # convert
-    voltages = hw.get_voltages(samples)
-    amplitude = hw.get_amplitude(voltages)
-    dc = hw.get_dc(voltages)
-    rms = hw.get_rms(voltages)
-    # get dft
-    dft_freqs, dft_mags = hw.get_dft(voltages)
-    # show params
+    clean_snr = hw.get_snr(clean_dft_mags)
+    dirty_snr = hw.get_snr(dirty_dft_mags)
+
+    # print out
     hw.dump_params()
-    # print
-    print("Amplitude: " + str(amplitude))
-    print("DC: " + str(dc))
-    print("RMS: " + str(rms))
-    print("RMS Noise: "+ str(noise_rms))
-    print("SNR: " + str((rms**2)/(noise_rms**2)))
+
+    print('-'*20 + " Ambient Results " + '-'*20)
+    print("*  Amp(V): " + str(ambient_amp))
+    print("*   DC(V): " + str(ambient_dc))
+    print("*  RMS(V): " + str(ambient_rms))
+    print("*  Max(V): " + str(max(ambient_voltages)))
+    print("*  Min(V): " + str(min(ambient_voltages)))
+    print('-'*20 + " Clean Results " + '-'*20)
+    print("*  Amp (V): " + str(clean_amp))
+    print("*   DC (V): " + str(clean_dc))
+    print("*  RMS (V): " + str(clean_rms))
+    print("*  Max (V): " + str(max(clean_voltages)))
+    print("*  Min (V): " + str(min(clean_voltages)))
+    print("*  SNR (dB): " + str(clean_snr))
+    print('-'*20 + " Noisy Results " + '-'*20)
+    print("*  Amp (V): " + str(dirty_amp))
+    print("*   DC (V): " + str(dirty_dc))
+    print("*  RMS (V): " + str(dirty_rms))
+    print("*  Max (V): " + str(max(dirty_voltages)))
+    print("*  Min (V): " + str(min(dirty_voltages)))
+    print("*  SNR (dB): " + str(dirty_snr))
+
     # plot
-    subplot(311)
-    plot(noise_voltages)
-    title("Noise")
-    subplot(312)
-    plot(voltages)
-    title("Hardware Data")
-    subplot(313)
-    plot(dft_freqs, dft_mags)
-    title("FFT")
+    subplot(321)
+    plot(ambient_voltages)
+    grid()
+    title("Ambient Data")
+    xlabel('Samples')
+    ylabel('Voltage(V)')
+    subplot(322)
+    plot(ambient_dft_freqs, ambient_dft_mags_log)
+    title("Ambient FFT")
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB)')
+    xscale('log')
+    grid()
+    
+    subplot(323)
+    plot(clean_voltages)
+    grid()
+    title("Clean Data")
+    xlabel('Samples')
+    ylabel('Voltage(V)')
+    subplot(324)
+    plot(clean_dft_freqs, clean_dft_mags_log)
+    title("Clean FFT")
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB)')
+    xscale('log')
+    grid()
+
+    subplot(325)
+    plot(dirty_voltages)
+    grid()
+    title("Noisy Data")
+    xlabel('Samples')
+    ylabel('Voltage(V)')
+    subplot(326)
+    plot(dirty_dft_freqs, dirty_dft_mags_log)
+    title("Noisy FFT")
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude (dB)')
+    xscale('log')
+    grid()
+
     tight_layout()
     show()
+    # close device
     hw.close_dad()
-        
+
+       
 hw = HwTest()
-oneshot(hw)
+#test_normal(hw)
+test_measurement(hw)
 
 #if(len(sys.argv) == 2 and sys.argv[1] == 'o'):
 #    oneshot_run(func_freq, func_amp, device, num_samples, signed, 0)
