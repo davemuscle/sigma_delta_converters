0f3c4e462e366733ffafb0732ff9d016435ef0fe
davemuscle <>
2022-02-03 21:56:26 -0500

    update model, attempting to figure out compensation filter

--- model/SigmaDeltaAdc.py
+++ model/SigmaDeltaAdc.py
@@ -6,28 +6,66 @@ from SigmaDeltaMod import *
 import numpy
 import scipy.signal
 import matplotlib.pyplot as pylab
+import math
 from matplotlib.widgets import Cursor
 
-# Comb/integrator taken from:
-# https://github.com/m1geo/CIC-Filter/blob/master/CIC-Filter.py
+def cic_decimator(samples, decimator, stages):
+    # integrator
+    yn = [0]*stages
+    ynm = [0]*stages
+    # copy to buffer
+    buf = []
+    for n in samples:
+        buf.append(n)
+    # perform integrator
+    for n in range(len(samples)):
+        for s in range(stages):
+            ynm[s] = yn[s]
+            yn[s] = ynm[s] + buf[n]
+            buf[n] = yn[s]
+    # downsample
+    dec = []
+    for n in range(0, len(samples), decimator):
+        dec.append(buf[n])
+    # comb
+    xn = [0]*stages
+    xnm = [0]*stages
+    # perform comb
+    for n in range(len(dec)):
+        for s in range(stages):
+            xnm[s] = xn[s]
+            xn[s] = dec[n]
+            dec[n] = xn[s] - xnm[s]
+    return dec
 
-class inte:
-    def __init__(self):
-        self.yn = 0;
-        self.ynm = 0
-    def update(self, inp):
-        self.ynm = self.yn
-        self.yn = self.ynm + inp
-        return self.yn
+def cic_compensator(samples, order):
+    if(order == 1):
+        A = -18
+    if(order == 2 or order == 3):
+        A = -10
+    if(order == 4 or order == 5):
+        A = -6
+    if(order >= 6):
+        A = -4
+    d1 = 0
+    d2 = 0
+    d = []
+    for x in samples:
+        d.append(d2 + x + A*d1)
+        d2 = d1
+        d1 = x
+    return d
 
-class comb:
-    def __init__(self):
-        self.xn = 0
-        self.xnm = 0;
-    def update(self, inp):
-        self.xnm = self.xn
-        self.xn = inp
-        return self.xn - self.xnm
+def cic_compensator_a(samples, a):
+    d1 = 0
+    d2 = 0
+    d = []
+    for x in samples:
+        y = (-1*a/2)*d2  + (1+a)*d1
+        d.append(y)
+        d2 = d1 + x
+        d1 = x
+    return d
 
 def generate_input(scale, num_samples, freq, sample_rate):
     x = [0]*num_samples
@@ -39,100 +77,105 @@ class SigmaDeltaAdc:
     _vcc = 0.0
 
     _bosr = 256
+    _cic_stages = 1
     _sclk = 44800
-    _num_samples = 4000
-    _freq = 110
+    _num_samples = 256
+    _freq = 440
+
+    _sweep_start = 220
+    _sweep_steps = 160
+    _sweep_mult = 2.0**(0.5/12.0)
 
     def __init__(self, vcc):
         self._vcc = vcc
         self._mod = SigmaDeltaMod(self._vcc)
-        self._analog_in = generate_input(0.9*self._vcc, self._num_samples*self._bosr, self._freq, self._sclk*self._bosr)
-        self._bits = [0] * self._num_samples*self._bosr
-        self._digital_out = [0] * self._num_samples
 
     def run(self):
+        analog_in = generate_input(0.9*self._vcc, self._num_samples*self._bosr, self._freq, self._sclk*self._bosr)
         # run analog input through sigma-delta mod
         for n in range(self._num_samples*self._bosr):
             # condition
-            self._analog_in[n] = (self._vcc/2) + (self._analog_in[n] / 2)
-            # run modulator to obtain bitstream
-            self._mod.set_analog(self._analog_in[n])
-            self._bits[n] = self._mod.loop_tick()
+            analog_in[n] = (self._vcc/2) + (analog_in[n] / 2)
 
-        # create taps for FIR
-        nyq_rate = self._sclk / 2
-        width = 3200/nyq_rate
-        ripple_db = 60
-        N, beta = scipy.signal.kaiserord(ripple_db, width)
-        cutoff_hz = 20000
-        taps = scipy.signal.firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-        #print("%d taps: " % N);
-        #for x in taps:
-        #    print("  " + str(float(x)));
+        # run modulator to obtain bitstream
+        # run cic decimator
+        digital_out = cic_decimator(self._mod.loop(analog_in), self._bosr, self._cic_stages)
+        # run compensator
+        digital_out = cic_compensator(digital_out, self._cic_stages)
+        # convert to voltage
+        analog_out = []
+        for x in bits_filtered:
+            analog_out.append(x * self._vcc / (self._bosr ** self._cic_stages))
+        # remove transient
+        for x in range(25):
+            analog_out.pop(0)
+        ## DC Removal
+        #yn = 0
+        #yn_mul = 0
+        #yn_reg = 0
+        #xn_reg = 0
+        #for n in range(self._num_samples):
+        #    #self._digital_out[n] = bits_filtered[n] - xn_reg + 0.995*yn_reg
+        #    #xn_reg = bits_filtered[n]
+        #    #yn_reg = self._digital_out[n]
 
-        # CIC filter
-        cic_stages = 1
-        intes = [inte() for a in range(cic_stages)]
-        combs = [comb() for a in range(cic_stages)]
-        n = 0
-        bits_filtered = [0] * self._num_samples
-        for k in range(self._num_samples*self._bosr):
-            z = self._bits[k]
-            # integrator
-            for g in range(cic_stages):
-                z = intes[g].update(z)
-            # comb
-            if((k % self._bosr)==0):
-                for g in range(cic_stages):
-                    z = combs[g].update(z)
-                    j = z
-                bits_filtered[n] = j / (self._bosr ** cic_stages)
-                n = n + 1
-       
-        # Why does a CIC filter on the bitstream give us the the correct output, but a normal FIR doesn't?
+        #    yn = bits_filtered[n] - yn_reg
+        #    yn_mul = yn * 0.005
+        #    self._digital_out[n] = self._vcc * yn
+        #    yn_reg = yn_mul + yn_reg
+        pylab.figure("Analog Input Data")
+        pylab.plot(analog_in)
+        pylab.figure("Digital Output Data")
+        pylab.plot(analog_out)
+        pylab.show()
 
-        # Low-pass final data
-        #self._digital_out = scipy.signal.lfilter(taps, 1.0, bits_filtered) 
+    def get_amplitude(self, samples):
+        low = samples[0]
+        high = samples[0]
+        for x in range(1, len(samples)):
+            if(samples[x] < low):
+                low = samples[x];
+            if(samples[x] > high):
+                high = samples[x];
+        return ((high-low)/2)
 
-        # 
-
-        # DC Removal
-        yn = 0
-        yn_mul = 0
-        yn_reg = 0
-        xn_reg = 0
-        for n in range(self._num_samples):
-            #self._digital_out[n] = bits_filtered[n] - xn_reg + 0.995*yn_reg
-            #xn_reg = bits_filtered[n]
-            #yn_reg = self._digital_out[n]
-
-            yn = bits_filtered[n] - yn_reg
-            yn_mul = yn * 0.005
-            self._digital_out[n] = self._vcc * yn
-            yn_reg = yn_mul + yn_reg
-
-        debug = 0
-
-        if(debug):
-            pylab.figure("debug taps")
-            pylab.plot(taps, 'bo-', linewidth=2)
-            pylab.title('Filter coefficients (%d taps)' % N)
-            pylab.grid(True)
-
-            pylab.figure("filter response (%d taps)" % N)
-            pylab.clf()
-            w,h = scipy.signal.freqz(taps, worN=8000)
-            pylab.plot((w/numpy.pi)*nyq_rate, numpy.absolute(h), linewidth=2)
-            pylab.xlabel('Freq (Hz)')
-            pylab.ylabel('Gain')
-            pylab.ylim(-0.05, 1.05)
-            pylab.grid(True)
+    def sweep(self):
+        freq = self._sweep_start
+        freqs = []
+        amps = []
+        amps_unc = []
+        gen_amp = 1.0
+        for x in range(self._sweep_steps):
+            freqs.append(freq)
+            analog_in = generate_input(gen_amp, self._num_samples * self._bosr, freq, self._sclk * self._bosr)
+            for n in range(len(analog_in)):
+                analog_in[n] += self._vcc / 2
+            digital_out = cic_decimator(self._mod.loop(analog_in), self._bosr, self._cic_stages)
+            analog_out = []
+            for x in digital_out:
+                analog_out.append(x * self._vcc / (self._bosr ** self._cic_stages))
+            analog_out_unc = []
+            for x in analog_out:
+                analog_out_unc.append(x)
+            analog_out = cic_compensator_a(analog_out, 0.2)
+            for x in range(25):
+                analog_out.pop(0)
+                analog_out_unc.pop(0)
+            amp = self.get_amplitude(analog_out)
+            amp_unc = self.get_amplitude(analog_out_unc)
+            print("Freq = " + str(freq) + ", Amp = " + str(amp), "AmpUnc = " + str(amp_unc))
+            amps.append(20*math.log10(amp / self.get_amplitude(analog_in)))
+            amps_unc.append(20*math.log10(amp_unc / self.get_amplitude(analog_in)))
+            freq *= self._sweep_mult
+        pylab.figure("Bode Plot Uncompensated")
+        pylab.plot(freqs, amps_unc)
+        pylab.xscale('log')
+        pylab.figure("Bode Plot")
+        pylab.plot(freqs, amps)
+        pylab.xscale('log')
+        pylab.show()
 
 x = SigmaDeltaAdc(2.5)
-x.run()
-pylab.figure("Analog Input Data")
-pylab.plot(x._analog_in)
-pylab.figure("Digital Output Data")
-pylab.plot(x._digital_out)
+#x.run()
+x.sweep()
 
-pylab.show()
--- model/SigmaDeltaMod.py
+++ model/SigmaDeltaMod.py
@@ -8,23 +8,12 @@ Sigma-Delta Modulator Class
 Usage:
   Create object:
       x = SigmaDeltaMod(vcc), vcc = float value of adc power pin
-  
-  Set input voltage:
-      x.set_analog(analog), analog = float
 
-  Run the loop, corresponding to the bit clk:
-      x.loop_tick(), returns 0 or 1 as part of the bitstream
-
-
-Debug:
-   Run modulator loop i,j times and prints digital values / bitstream:
-      x.test(i, j)
-   i corresponds to number of bits to count for comprising a digital sample
-   j corresponds to number of samples to gather
-
+  Run the loop on input samples, corresponding to the bit clk:
+      x.loop(), returns output of SD modulator
 """
+
 class SigmaDeltaMod:
-    _avalue = 0.0
     _intvalue = 0.0
     _summer = 0.0
     _impulse = 0
@@ -35,29 +24,19 @@ class SigmaDeltaMod:
     def __init__(self, vcc):
         self._vcc = vcc
 
-    def set_analog(self, float):
-        self._avalue = float
-
-    def loop_tick(self):
-        # adder
-        self._summer = self._avalue - self._afeedback
-        # integrator
-        self._intvalue = self._intvalue + self._summer
-        # comparator 
-        if(self._intvalue > 0.0):
-            self._impulse = 1
-        else:
-            self._impulse = 0
-        # feedback
-        self._afeedback = self._vcc * self._impulse
-        return self._impulse
-
-    def test(self, bits, samples):
-        for i in range(samples):
-            d = 0
-            print("  ", end='')
-            for j in range(bits):
-                v = self.loop_tick()
-                d = v + d
-                print(str(v) + "", end='')
-            print(" -> total = " + str(d), "-> voltage = " + str(self._vcc*d/bits))
+    def loop(self, samples):
+        y = []
+        for x in samples:
+            # adder
+            self._summer = x - self._afeedback
+            # integrator
+            self._intvalue = self._intvalue + self._summer
+            # comparator 
+            if(self._intvalue > 0.0):
+                self._impulse = 1
+            else:
+                self._impulse = 0
+            # feedback
+            self._afeedback = self._vcc * self._impulse
+            y.append(self._impulse)
+        return y
